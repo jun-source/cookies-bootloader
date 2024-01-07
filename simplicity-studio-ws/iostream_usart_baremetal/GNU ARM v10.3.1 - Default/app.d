99fe294e0c1d4246b1e6722b6873f1f1201f1089
jun <jzhu6751@gmail.com>
2024-01-07 23:23:28 +0100

    Remove iostream_usart_baremetal and add script to launch simplicity
    Signed-off-by: jun <jzhu6751@gmail.com>

--- simplicity-studio-ws/iostream_usart_baremetal/GNU ARM v10.3.1 - Default/app.d
+++
@@ -1,5 +0,0 @@
-app.o: ../app.c \
- /home/jun/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h \
- ../app_iostream_usart.h
-/home/jun/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h:
-../app_iostream_usart.h:
--- simplicity-studio-ws/iostream_usart_baremetal/GNU ARM v10.3.1 - Default/gecko_sdk_4.3.2/platform/common/src/sl_string.d
+++
@@ -1,6 +0,0 @@
-gecko_sdk_4.3.2/platform/common/src/sl_string.o: \
- /home/jun/SimplicityStudio/SDKs/gecko_sdk/platform/common/src/sl_string.c \
- /home/jun/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h \
- /home/jun/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc/sl_string.h
-/home/jun/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h:
-/home/jun/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc/sl_string.h:
