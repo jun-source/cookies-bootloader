a218a7a37d96ce3ebd44b70da53e274d5f8ee42f
jun <jzhu6751@gmail.com>
2024-01-07 22:54:08 +0100

    Remove bootloader-uart-bgapi project and add script to launch simplicity
    Signed-off-by: jun <jzhu6751@gmail.com>

--- simplicity-studio-ws/bootloader-uart-bgapi/GNU ARM v10.3.1 - Default/gecko_sdk_4.3.2/platform/bootloader/security/btl_crc16.d
+++
@@ -1,6 +0,0 @@
-gecko_sdk_4.3.2/platform/bootloader/security/btl_crc16.o: \
- /home/jun/SimplicityStudio/SDKs/gecko_sdk/platform/bootloader/security/btl_crc16.c \
- /home/jun/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h \
- /home/jun/SimplicityStudio/SDKs/gecko_sdk/platform/bootloader/security/btl_crc16.h
-/home/jun/SimplicityStudio/SDKs/gecko_sdk//platform/common/toolchain/inc/sl_gcc_preinclude.h:
-/home/jun/SimplicityStudio/SDKs/gecko_sdk/platform/bootloader/security/btl_crc16.h:
---
+++ simplicity-studio-ws/launch-simplicity.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/$USER/SimplicityStudio_v5/studiowayland.sh
