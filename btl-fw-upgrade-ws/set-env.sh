4e5003ff983a91ce46c602a8e93801bfcae164aa
juntian <jzhu6751@gmail.com>
2024-05-19 18:11:07 +0200

    set-env.sh: warn users to check toolchain
    Simplicity Studio gets new releases from time to time. Therefore,
    some resources like the toolchain might be different from the one
    used when this project was started which is gnu_arm/10.3_2021.10.
    If the user has downloaded the latest, check the version and change
    PATH_GCCARM accordingly.
    
    Signed-off-by: juntian <jzhu6751@gmail.com>

--- btl-fw-upgrade-ws/set-env.sh
+++ btl-fw-upgrade-ws/set-env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export PATH_SCMD=/home/$USER/SimplicityStudio_v5/developer/adapter_packs/commander
+echo "Check the toolchain version at /home/$USER/SimplicityStudio_v5/developer/toolchains/gnu_arm/ and change PATH_GCCARM env var in this file if it does not match with 10.3_2021.10"
 export PATH_GCCARM=/home/$USER/SimplicityStudio_v5/developer/toolchains/gnu_arm/10.3_2021.10
 export DEVICE=EFR32BG12P132F1024GL125
